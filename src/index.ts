0506ca7ba569e353ec0723a1f54c8e5575d5d6b8
HORDEQUIN Alexandre (SNCF RESEAU / EIC LANGUEDOC ROUSSILLON / NARBONNE) <7809188y@commun.ad.sncf.fr>
2023-01-23 12:13:42 +0100

    merge

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { Users } from "./entity/Users"
 import { Restaurant } from "./entity/Restaurant"
 import { Menu } from "./entity/Menu"
 
